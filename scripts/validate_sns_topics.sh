f5042e75778420bb9a3021122785a0bf9d5ce43d
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-12-02 13:01:09 -0600

    [nc] iterating on pipeline run
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/validate_sns_topics.sh
+++ scripts/validate_sns_topics.sh
@@ -3,6 +3,7 @@
 export ACCOUNT=$1
 export AWS_ACCOUNT_ID=$(cat ${ACCOUNT}.auto.tfvars.json | jq -r .aws_account_id)
 export AWS_ASSUME_ROLE=$(cat ${ACCOUNT}.auto.tfvars.json | jq -r .aws_assume_role)
+export AWS_DEFAULT_REGION=$(cat ${ACCOUNT}.auto.tfvars.json | jq -r .aws_region)
 
 if [[ $(aws sns publish --topic-arn arn:aws:sns:us-east-2:${AWS_ACCOUNT_ID}:slack-lab-events-topic --message "Pipeline test for SNS topic = Slack #lab-events channel" | grep MessageId) != "" ]]; then
   echo "Event Message Posted"
@@ -24,6 +25,6 @@ aws sts assume-role --output json --role-arn arn:aws:iam::${AWS_ACCOUNT_ID}:role
 export AWS_ACCESS_KEY_ID=$(cat credentials | jq -r ".Credentials.AccessKeyId")
 export AWS_SECRET_ACCESS_KEY=$(cat credentials | jq -r ".Credentials.SecretAccessKey")
 export AWS_SESSION_TOKEN=$(cat credentials | jq -r ".Credentials.SessionToken")
-export AWS_DEFAULT_REGION=$(cat ${ACCOUNT}.auto.tfvars.json | jq -r .aws_region)
+
 
 rspec spec/*
